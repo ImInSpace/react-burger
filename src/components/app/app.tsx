b7d61bdc8f3a0ae81e5e6d11a97560631d83969c
Vyacheslav Kirichenko <maker1992@mail.ru>
2023-08-27 15:15:08 +0300

    Перенёс App в components

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import { AppBody } from "../body/app-body/app-body";
+import { AppHeader } from "../header/app-header/app-header";
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+      <AppBody />
+    </>
+  );
+}
+
+export { App };
