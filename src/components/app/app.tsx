3337c5cf9a17bb7c5b3dc65122c4d979b2138af9
Vyacheslav Kirichenko <maker1992@mail.ru>
2023-08-29 22:57:14 +0300

    Вернул стандартный вывод страницы.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,23 +3,19 @@ import styles from "./app.module.css";
 import { BurgerIngredients } from "../burger-ingredients/burger-ingredients";
 import { BurgerConstructor } from "../burger-constructor/burger-constructor";
 import { CreateOrder } from "../burger-constructor/create-order/create-order";
-import { OrderInfo } from "../order-info/order-info";
-import { IngredientDetails } from "../ingredient-details/ingredient-details";
 
 function App() {
   return (
     <>
       <AppHeader />
-      <IngredientDetails />
-      {/* <OrderInfo /> */}
       <div className={styles.container}>
-        {/* <div className={styles.halfContainer}>
+        <div className={styles.halfContainer}>
           <BurgerIngredients />
         </div>
         <div className={styles.halfContainer}>
           <BurgerConstructor />
           <CreateOrder />
-        </div> */}
+        </div>
       </div>
     </>
   );
