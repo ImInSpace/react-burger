669b2f3c4f79c7f4d72d493ce6a7ef57799bc0c2
Кириченко Вячеслав <v.kirichenko@vniias.ru>
2023-08-29 12:20:54 +0300

    core-review. Удалил излишнюю декомпозицию компонетов меню.

--- src/components/header/app-header/app-header.js
+++ src/components/header/app-header/app-header.js
@@ -1,15 +1,24 @@
-import { HeaderMenu } from "../header-menu/header-menu";
-import { LoginBtn } from "../login-btn/login-btn";
-import { HeaderLogo } from "../header-logo/header-logo";
 import styles from "./app-header.module.css";
+import { MenuItem } from "../menu-item/menu-item";
+import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ListIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 function AppHeader() {
   return (
     <div className={styles.header}>
       <div className={styles.container}>
-        <HeaderMenu />
-        <HeaderLogo />
-        <LoginBtn />
+        <div className={styles.leftMenu}>
+          <MenuItem text="Конструктор" url={"#"} icon={<BurgerIcon />} />
+          <MenuItem text="Лента заказов" url={"#"} icon={<ListIcon />} />
+        </div>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <div className={styles.btn}>
+          <MenuItem text="Личный кабинет" url={"#"} icon={<ProfileIcon />} />
+        </div>
       </div>
     </div>
   );
