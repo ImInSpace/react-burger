2f4c3882fcf58868542d062f7c1b9831ad92293c
Vyacheslav Kirichenko <maker1992@mail.ru>
2023-08-29 21:29:40 +0300

    Добавлен компонент информация о заказе.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,19 +3,21 @@ import styles from "./app.module.css";
 import { BurgerIngredients } from "../burger-ingredients/burger-ingredients";
 import { BurgerConstructor } from "../burger-constructor/burger-constructor";
 import { CreateOrder } from "../burger-constructor/create-order/create-order";
+import { OrderInfo } from "../order-info/order-info";
 
 function App() {
   return (
     <>
       <AppHeader />
       <div className={styles.container}>
-        <div className={styles.halfContainer}>
+        <OrderInfo />
+        {/* <div className={styles.halfContainer}>
           <BurgerIngredients />
         </div>
         <div className={styles.halfContainer}>
           <BurgerConstructor />
           <CreateOrder />
-        </div>
+        </div> */}
       </div>
     </>
   );
---
+++ src/components/order-info/order-info.js
@@ -0,0 +1,26 @@
+import styles from "./order-info.module.css";
+import doneIcon from "../../images/done.png";
+import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
+function OrderInfo() {
+  return (
+    <div class={styles.container}>
+      <div className={styles.shadow}>
+        <CloseIcon />
+      </div>
+      <p className={styles.orderNumber + " text text_type_digits-large mt-30"}>
+        034536
+      </p>
+      <p className="text text_type_main-medium mt-8">Идентификатор заказа</p>
+      <img className="mt-15" src={doneIcon} alt="order icon" />
+      <p className="text text_type_main-default mt-15">
+        Ваш заказ начали готовить
+      </p>
+      <p className={styles.waitForOrder + " text text_type_main-default mt-2"}>
+        Дождитесь готовности на орбитальной станции
+      </p>
+    </div>
+  );
+}
+
+export { OrderInfo };
