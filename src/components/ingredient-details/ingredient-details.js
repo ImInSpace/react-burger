5b8bca7489bc1a269b97f767cc7f609aad71c23a
Кириченко Вячеслав <v.kirichenko@vniias.ru>
2023-09-04 15:36:32 +0300

    Fix. Refactoring.

--- src/components/ingredient-details/ingredient-details.js
+++ src/components/ingredient-details/ingredient-details.js
@@ -1,7 +1,7 @@
 import styles from "./ingredient-details.module.css";
 import { IngredientImage } from "./ingredient-image/ingredient-image";
-import { IngredientTitle } from "./components/ingredient-title/ingredient-title";
-import { Macronutrients } from "./components/macronutrients/macronutrients";
+import { IngredientTitle } from "./ingredient-title/ingredient-title";
+import { Macronutrients } from "./macronutrients/macronutrients";
 import { ingredientDataShape } from "../../utils/prop-types";
 
 function IngredientDetails({ data }) {
