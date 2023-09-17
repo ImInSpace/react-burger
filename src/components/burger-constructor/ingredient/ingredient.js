8fbdc72c3b788f5b30c1313c29cb8fdcfea5d92d
Vyacheslav Kirichenko <maker1992@mail.ru>
2023-09-17 14:36:29 +0300

    Курсор в bc заменен на grab.

--- src/components/burger-constructor/ingredient/ingredient.js
+++ src/components/burger-constructor/ingredient/ingredient.js
@@ -4,12 +4,74 @@ import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-comp
 import { useDispatch } from "react-redux";
 import { REMOVE_INGREDIENT } from "../../../services/actions/ingredients";
 import PropTypes from "prop-types";
+import { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import styles from "./ingredient.module.css";
 
 function Ingredient({ ingredientInfo, index }) {
   const dispatch = useDispatch();
+  const draggableRowRef = useRef(null);
+
+  const [{ handlerId }, drop] = useDrop({
+    accept: "ingredient",
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item, monitor) {
+      if (!draggableRowRef.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect =
+        draggableRowRef.current?.getBoundingClientRect();
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      // Time to actually perform the action
+      //moveCard(dragIndex, hoverIndex);
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "ingredient",
+    item: () => {
+      return { id: ingredientInfo._id, index };
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
 
   return (
-    <div>
+    <div ref={draggableRowRef} draggable className={styles.dragPointer}>
       <ConstructorRow showDragIcon={true}>
         <ConstructorElement
           text={ingredientInfo.name}
