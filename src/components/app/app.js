7c6efcf8ca9e2912a8ee4c6824bc53f755b9453e
Vyacheslav Kirichenko <maker1992@mail.ru>
2023-10-01 14:04:23 +0300

    Вынес главную страницу в HomePage.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,66 +1,15 @@
-import { AppHeader } from "../app-header/app-header";
-import styles from "./app.module.css";
-import { BurgerIngredients } from "../burger-ingredients/burger-ingredients";
-import { BurgerConstructor } from "../burger-constructor/burger-constructor";
-import { CreateOrder } from "../ui/create-order/create-order";
-import { IngredientDetails } from "../ingredient-details/ingredient-details";
-import { OrderDetails } from "../order-details/order-details";
-import { useEffect } from "react";
-import { Modal } from "../ui/modal/modal";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addIngredient,
-  loadIngredients,
-} from "../../services/actions/ingredients";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../../pages/home";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadIngredients());
-  }, [dispatch]);
-
-  const selectedIngredient = useSelector(
-    (store) => store.ingredients.selectedIngredient
-  );
-
-  const isCreateOrderModalShown = useSelector(
-    (store) => store.order.isModalShown
-  );
-
-  const handleDrop = (dragItem) => {
-    dispatch(addIngredient(dragItem.id));
-  };
-
   return (
-    <div className={styles.app}>
-      <AppHeader />
-      <DndProvider backend={HTML5Backend}>
-        <div className={styles.container}>
-          <div className={styles.halfContainer}>
-            <BurgerIngredients />
-          </div>
-          <div className={styles.halfContainer}>
-            <BurgerConstructor onDropHandler={handleDrop} />
-            <CreateOrder />
-          </div>
-        </div>
-      </DndProvider>
-
-      {isCreateOrderModalShown && (
-        <Modal>
-          <OrderDetails />
-        </Modal>
-      )}
-
-      {selectedIngredient && (
-        <Modal caption={"Детали инредиента"}>
-          <IngredientDetails data={selectedIngredient} />
-        </Modal>
-      )}
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,65 @@
+import styles from "./home.module.css";
+import { AppHeader } from "../components/app-header/app-header";
+import { BurgerIngredients } from "../components/burger-ingredients/burger-ingredients";
+import { BurgerConstructor } from "../components/burger-constructor/burger-constructor";
+import { CreateOrder } from "../components/ui/create-order/create-order";
+import { IngredientDetails } from "../components/ingredient-details/ingredient-details";
+import { OrderDetails } from "../components/order-details/order-details";
+import { useEffect } from "react";
+import { Modal } from "../components/ui/modal/modal";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addIngredient,
+  loadIngredients,
+} from "../services/actions/ingredients";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+export default function HomePage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadIngredients());
+  }, [dispatch]);
+
+  const selectedIngredient = useSelector(
+    (store) => store.ingredients.selectedIngredient
+  );
+
+  const isCreateOrderModalShown = useSelector(
+    (store) => store.order.isModalShown
+  );
+
+  const handleDrop = (dragItem) => {
+    dispatch(addIngredient(dragItem.id));
+  };
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <DndProvider backend={HTML5Backend}>
+        <div className={styles.container}>
+          <div className={styles.halfContainer}>
+            <BurgerIngredients />
+          </div>
+          <div className={styles.halfContainer}>
+            <BurgerConstructor onDropHandler={handleDrop} />
+            <CreateOrder />
+          </div>
+        </div>
+      </DndProvider>
+
+      {isCreateOrderModalShown && (
+        <Modal>
+          <OrderDetails />
+        </Modal>
+      )}
+
+      {selectedIngredient && (
+        <Modal caption={"Детали инредиента"}>
+          <IngredientDetails data={selectedIngredient} />
+        </Modal>
+      )}
+    </div>
+  );
+}
