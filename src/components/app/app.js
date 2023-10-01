f6257873c12d561c47646d558641cb0602f9ee3c
Vyacheslav Kirichenko <maker1992@mail.ru>
2023-10-01 18:32:43 +0300

    Сверстал экраны восстановления и сброса пароля.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,17 +3,19 @@ import HomePage from "../../pages/home";
 import LoginPage from "../../pages/login";
 import RegisterPage from "../../pages/register";
 import ForgotPasswordPage from "../../pages/forgot-password";
+import ResetPasswordPage from "../../pages/reset-password";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           {/* prettier-ignore */}
-          <Route path="/forgot-password" element={<ForgotPasswordPage />}></Route>
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/reset-password.js
@@ -0,0 +1,77 @@
+import styles from "./reset-password.module.css";
+import { AppHeader } from "../components/app-header/app-header";
+import cn from "classnames";
+import {
+  Input,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+
+export default function ResetPasswordPage() {
+  const [password, setPassword] = useState("");
+  const passwordInputRef = useRef(null);
+  const onIconPasswordClick = () => {
+    setTimeout(() => passwordInputRef.current.focus(), 0);
+    alert("Icon Click Callback");
+  };
+
+  const [codeFromEmail, setCodeFromEmail] = useState("");
+  const codeFromEmailInputRef = useRef(null);
+
+  return (
+    <>
+      <AppHeader />
+      <div className={styles.root}>
+        <div className={styles.container}>
+          {/* prettier-ignore */}
+          <p className="text text_type_main-medium">Восстановление пароля</p>
+          <Input
+            type={"text"}
+            placeholder={"Имя"}
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            name={"emailCode"}
+            error={false}
+            onIconClick={onIconPasswordClick}
+            ref={codeFromEmailInputRef}
+            errorText={"Ошибка"}
+            size={"default"}
+            extraClass="mt-6"
+          />
+          <Input
+            type={"email"}
+            placeholder={"email"}
+            onChange={(e) => setCodeFromEmail(e.target.value)}
+            value={codeFromEmail}
+            name={"email"}
+            error={false}
+            ref={codeFromEmailInputRef}
+            errorText={"Ошибка"}
+            size={"default"}
+            extraClass="mt-6"
+          />
+          <div className={styles.loginButton}>
+            <Button htmlType="button" type="primary" size="medium">
+              Сохранить
+            </Button>
+          </div>
+          <div className={cn(styles.registrationContainer, "mt-28")}>
+            {/* prettier-ignore */}
+            <span className={cn("text text_type_main-default", styles.gray)}>Вспомнили пароль?</span>
+            <Link className={styles.link} to="#">
+              <span
+                className={cn(
+                  "text text_type_main-default",
+                  styles.registerLink
+                )}
+              >
+                Войти
+              </span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
