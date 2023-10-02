c933f21d8544998dcfd6a8555afd00ebba201d1a
Vyacheslav Kirichenko <maker1992@mail.ru>
2023-10-02 23:14:37 +0300

    Вынес пункты меню в компоненты.

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,30 +1,25 @@
 import { AppHeader } from "../components/app-header/app-header";
 import styles from "./profile.module.css";
-import { Link } from "react-router-dom";
-import { EditProfile } from "../components/edit-profile";
+import { EditProfile } from "../components/profile/edit-profile";
+import { useState } from "react";
+import { MenuItem } from "../components/profile/menu-item";
 
 export default function Profile() {
+  const [activeMenu, setActiveMenu] = useState([true, false, false]);
+
+  const onMenuClickHandler = () => {
+    // ToDo: Переключатель меню.
+  };
+
   return (
     <>
       <AppHeader />
       <div className={styles.root}>
         <div className={styles.container}>
           <div className="menu">
-            <div className={styles.menuCell}>
-              <Link to="#" className={styles.link}>
-                <p className="text text_type_main-medium">Профиль</p>
-              </Link>
-            </div>
-            <div className={styles.menuCell}>
-              <Link to="#" className={styles.link}>
-                <p className="text text_type_main-medium">История заказов</p>
-              </Link>
-            </div>
-            <div className={styles.menuCell}>
-              <Link to="#" className={styles.link}>
-                <p className="text text_type_main-medium">Выход</p>
-              </Link>
-            </div>
+            <MenuItem caption="Профиль" link="#" isActive={true} />
+            <MenuItem caption="История заказов" link="#" isActive={false} />
+            <MenuItem caption="Выход" link="#" isActive={false} />
           </div>
           <div className={styles.subMenu}>
             <EditProfile />
