dc4b24b671e5acc2e5e479d8956dc9e6e05cb5dd
Кириченко Вячеслав <v.kirichenko@vniias.ru>
2023-10-03 09:57:00 +0300

    Реализовано подсвечивание активного меню в профиле при клике.

--- src/components/profile/menu-item/index.js
+++ src/components/profile/menu-item/index.js
@@ -4,10 +4,10 @@ import PropTypes from "prop-types";
 import cn from "classnames";
 
 function MenuItem(props) {
-  const { link, isActive, caption } = props;
+  const { link, isActive, caption, onClickHandler } = props;
 
   return (
-    <div className={styles.menuCell}>
+    <div className={styles.menuCell} onClick={onClickHandler}>
       <Link to={link} className={styles.link}>
         {/* prettier-ignore */}
         <p className={cn("text text_type_main-medium ", isActive ? "" : "text_color_inactive")}>
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -5,10 +5,14 @@ import { useState } from "react";
 import { MenuItem } from "../components/profile/menu-item";
 
 export default function Profile() {
-  const [activeMenu, setActiveMenu] = useState([true, false, false]);
+  const initialState = [true, false, false];
+  const [activeMenu, setActiveMenu] = useState(initialState);
 
-  const onMenuClickHandler = () => {
-    // ToDo: Переключатель меню.
+  const onMenuClickHandler = (index) => {
+    console.log("clicked: ", index);
+    const newState = [false, false, false];
+    newState[index] = true;
+    setActiveMenu(newState);
   };
 
   return (
@@ -17,9 +21,24 @@ export default function Profile() {
       <div className={styles.root}>
         <div className={styles.container}>
           <div className="menu">
-            <MenuItem caption="Профиль" link="#" isActive={true} />
-            <MenuItem caption="История заказов" link="#" isActive={false} />
-            <MenuItem caption="Выход" link="#" isActive={false} />
+            <MenuItem
+              caption="Профиль"
+              link="#"
+              isActive={activeMenu[0]}
+              onClickHandler={() => onMenuClickHandler(0)}
+            />
+            <MenuItem
+              caption="История заказов"
+              link="#"
+              isActive={activeMenu[1]}
+              onClickHandler={() => onMenuClickHandler(1)}
+            />
+            <MenuItem
+              caption="Выход"
+              link="#"
+              isActive={activeMenu[2]}
+              onClickHandler={() => onMenuClickHandler(2)}
+            />
           </div>
           <div className={styles.subMenu}>
             <EditProfile />
