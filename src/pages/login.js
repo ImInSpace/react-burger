a3f17a6120ff44a53801bf2d25c7915806ab7568
Кириченко Вячеслав <v.kirichenko@vniias.ru>
2023-10-10 13:26:59 +0300

    Удалены пустые скобки в jsx разметке в LoginPage.

--- src/pages/login.js
+++ src/pages/login.js
@@ -31,52 +31,50 @@ export default function LoginPage() {
   };
 
   return (
-    <>
-      <div className={styles.root}>
-        <form className={styles.container} onSubmit={login}>
+    <div className={styles.root}>
+      <form className={styles.container} onSubmit={login}>
+        {/* prettier-ignore */}
+        <p className="text text_type_main-medium">Вход</p>
+        <EmailInput
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          name={"email"}
+          isIcon={false}
+          extraClass="mt-6"
+        />
+        <PasswordInput
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          name={"password"}
+          extraClass="mt-6"
+        />
+        <div className={styles.loginButton}>
+          <Button htmlType="submit" type="primary" size="medium">
+            Войти
+          </Button>
+        </div>
+        <div className={cn(styles.registrationContainer, "mt-28")}>
           {/* prettier-ignore */}
-          <p className="text text_type_main-medium">Вход</p>
-          <EmailInput
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            name={"email"}
-            isIcon={false}
-            extraClass="mt-6"
-          />
-          <PasswordInput
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            name={"password"}
-            extraClass="mt-6"
-          />
-          <div className={styles.loginButton}>
-            <Button htmlType="submit" type="primary" size="medium">
-              Войти
-            </Button>
-          </div>
-          <div className={cn(styles.registrationContainer, "mt-28")}>
-            {/* prettier-ignore */}
-            <span className={cn("text text_type_main-default", styles.gray)}>
+          <span className={cn("text text_type_main-default", styles.gray)}>
             Вы - новый пользователь?
           </span>
-            <Link to="/register" className={styles.link}>
-              {/* prettier-ignore */}
-              <span className={cn("text text_type_main-default", styles.registerLink)}>
+          <Link to="/register" className={styles.link}>
+            {/* prettier-ignore */}
+            <span className={cn("text text_type_main-default", styles.registerLink)}>
               Зарегистрироваться
             </span>
-            </Link>
-          </div>
-          <div className={cn(styles.registrationContainer, "mt-6")}>
-            {/* prettier-ignore */}
-            <span className={cn("text text_type_main-default", styles.gray)}>Забыли пароль?</span>
-            <Link to="/forgot-password" className={styles.link}>
-              <span className="text text_type_main-default">
-                Восстановить пароль
-              </span>
-            </Link>
-          </div>
-        </form>
-      </div>
-    </>
+          </Link>
+        </div>
+        <div className={cn(styles.registrationContainer, "mt-6")}>
+          {/* prettier-ignore */}
+          <span className={cn("text text_type_main-default", styles.gray)}>Забыли пароль?</span>
+          <Link to="/forgot-password" className={styles.link}>
+            <span className="text text_type_main-default">
+              Восстановить пароль
+            </span>
+          </Link>
+        </div>
+      </form>
+    </div>
   );
 }
